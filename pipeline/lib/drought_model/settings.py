8d388d34b4352689b2a3c6acc8c6ffa1eb8434a7
aklilu <akliludin@gmail.com>
2022-06-09 09:18:06 +0200

    upload script to upload to ibf test ethiopia

--- pipeline/lib/drought_model/settings.py
+++ pipeline/lib/drought_model/settings.py
@@ -3,53 +3,46 @@
 ##################
 
 # 1. Try to load secrets from Azure key vault (i.e. when running through Logic App) if user has access
-from datetime import date
+from datetime import date,timedelta
 from dateutil.relativedelta import relativedelta
 
-try:
-    from azure.identity import DefaultAzureCredential
-    from azure.keyvault.secrets import SecretClient
+# try:
+    # from azure.identity import DefaultAzureCredential
+    # from azure.keyvault.secrets import SecretClient
 
-    az_credential = DefaultAzureCredential()
-    secret_client = SecretClient(
-        vault_url="https://ibf-flood-keys.vault.azure.net", credential=az_credential
-    )
+    # az_credential = DefaultAzureCredential()
+    # secret_client = SecretClient(
+        # vault_url="https://ibf-flood-keys.vault.azure.net", credential=az_credential
+    # )
 
-    ADMIN_LOGIN = secret_client.get_secret("ADMIN-LOGIN").value
-    GLOFAS_USER = secret_client.get_secret("GLOFAS-USER").value
-    GLOFAS_PW = secret_client.get_secret("GLOFAS-PW").value
-    GOOGLE_DRIVE_DATA_URL = secret_client.get_secret("GOOGLE-DRIVE-DATA-URL").value
-    UGA_URL = secret_client.get_secret("UGA-URL").value
-    ZMB_URL = secret_client.get_secret("ZMB-URL").value
-    ETH_URL = secret_client.get_secret("ETH-URL").value
-    KEN_URL = secret_client.get_secret("KEN-URL").value
-    UGA_PASSWORD = secret_client.get_secret("UGA-PASSWORD").value
-    ZMB_PASSWORD = secret_client.get_secret("ZMB-PASSWORD").value
-    ETH_PASSWORD = secret_client.get_secret("ETH-PASSWORD").value
-    KEN_PASSWORD = secret_client.get_secret("KEN-PASSWORD").value
+    # ADMIN_LOGIN = secret_client.get_secret("ADMIN-LOGIN").value    
+    # IBF_URL=secret_client.get_secret("IBF-URL").value    
+    # IBF_PASSWORD=secret_client.get_secret("IBF-PASSWORD").value    
 
-except Exception as e:
-    print("No access to Azure Key vault, skipping.")
+# except Exception as e:
+    # print("No access to Azure Key vault, skipping.")
 
 # 2. Try to load secrets from env-variables (i.e. when using Github Actions)
-try:
-    import os
+# try:
+    # import os
 
-    ADMIN_LOGIN = os.environ["ADMIN_LOGIN"]
-    GLOFAS_USER = os.environ["GLOFAS_USER"]
-    GLOFAS_PW = os.environ["GLOFAS_PW"]
-    GOOGLE_DRIVE_DATA_URL = os.environ["GOOGLE_DRIVE_DATA_URL"]
-    UGA_URL = os.environ["UGA_URL"]
-    ZMB_URL = os.environ["ZMB_URL"]
-    ETH_URL = os.environ["ETH_URL"]
-    KEN_URL = os.environ["KEN_URL"]
-    UGA_PASSWORD = os.environ["UGA_PASSWORD"]
-    ZMB_PASSWORD = os.environ["ZMB_PASSWORD"]
-    ETH_PASSWORD = os.environ["ETH_PASSWORD"]
-    KEN_PASSWORD = os.environ["KEN_PASSWORD"]
+    # ADMIN_LOGIN = os.environ["ADMIN_LOGIN"]
+    # GLOFAS_USER = os.environ["GLOFAS_USER"]
+    # GLOFAS_PW = os.environ["GLOFAS_PW"]
+    # GOOGLE_DRIVE_DATA_URL = os.environ["GOOGLE_DRIVE_DATA_URL"]
+    # IBF_URL=os.environ['IBF_API_URL']
+    # UGA_URL = os.environ["UGA_URL"]
+    # ZMB_URL = os.environ["ZMB_URL"]
+    # ETH_URL = os.environ["ETH_URL"]
+    # KEN_URL = os.environ["KEN_URL"]
+    # IBF_PASSWORD=os.environ['IBF_PASSWORD']
+    # UGA_PASSWORD = os.environ["UGA_PASSWORD"]
+    # ZMB_PASSWORD = os.environ["ZMB_PASSWORD"]
+    # ETH_PASSWORD = os.environ["ETH_PASSWORD"]
+    # KEN_PASSWORD = os.environ["KEN_PASSWORD"]
 
-except Exception as e:
-    print("No environment variables found.")
+#except Exception as e:
+ #   print("No environment variables found.")
 
 # 3. If 1. and 2. both fail, then assume secrets are loaded via secrets.py file (when running locally). If neither of the 3 options apply, this script will fail.
 try:
@@ -63,10 +56,11 @@ except ImportError:
 ######################
 
 # Countries to include
-# COUNTRY_CODES = ['KEN']#,'ZMB','ETH','UGA']
-COUNTRY_CODES = {"ETH": {"seasons": [2, 3, 4]}}  #'KEN':{'seasons':[1]},
 
-CROPPING_ZONES = [2, 3, 4]
+COUNTRY_CODES = {'ETH': {'seasons': [2, 3, 4]}}#,'KEN':{'seasons':[1]},}
+
+
+#CROPPING_ZONES ethiopia = [2, 3, 4]
 # Meher     1
 # Belg      2
 # Southern  4
@@ -74,12 +68,13 @@ CROPPING_ZONES = [2, 3, 4]
 
 SETTINGS = {
     "ETH": {
-        "IBF_API_URL": ETH_URL,
-        "PASSWORD": ETH_PASSWORD,
-        "TRIGGER_PROBABILITY": 45,
-        "TRIGGER_SCENARIO": "trigger_treshold_one",
-        "TRIGGER_threshold": [-1, 30],
-        "TRIGGER_rain_prob_threshold": [45, 30],
+        "IBF_API_URL": IBF_URL,
+        "PASSWORD": IBF_PASSWORD,
+        "TRIGGER_PROBABILITY": 25,#45
+        "TRIGGER_SCENARIO": "Trigger_threshold_spi_obs",
+        "TRIGGER_threshold": [-1, 10],
+        "TRIGGER_rain_prob_threshold": [45, 1],
+        "SPI_Threshold_Prob" : 'NA',
         "mock": False,
         "if_mock_trigger": False,
         "notify_email": False,
@@ -142,12 +137,13 @@ SETTINGS = {
         },
     },
     "KEN": {
-        "IBF_API_URL": KEN_URL,
-        "TRIGGER_SCENARIO": "trigger_treshold_one",
+        "IBF_API_URL": IBF_URL,
+        "PASSWORD": IBF_PASSWORD,
         "TRIGGER_PROBABILITY": 40,
-        "TRIGGER_threshold": [-1, 30],
-        "TRIGGER_rain_prob_threshold": [45, 30],
-        "PASSWORD": KEN_PASSWORD,
+        "TRIGGER_SCENARIO": "trigger_treshold_one",
+        "TRIGGER_threshold": [-1, 30],          
+        "TRIGGER_rain_prob_threshold": [40, 40],
+        "SPI_Threshold_Prob" : '0.16354',         
         "mock": False,
         "if_mock_trigger": False,
         "notify_email": False,
@@ -155,15 +151,15 @@ SETTINGS = {
             1: {
                 1: ["1-month"],
                 2: ["0-month"],
-                3: ["7-month"],
-                4: ["6-month"],
-                5: ["5-month"],
-                6: ["4-month"],
-                7: ["3-month"],
-                8: ["2-month"],
-                9: ["1-month"],
+                3: ["0-month"],
+                4: ["0-month"],
+                5: ["4-month"],
+                6: ["3-month"],
+                7: ["2-month"],
+                8: ["1-month"],
+                9: ["0-month"],
                 10: ["0-month"],
-                11: ["3-month"],
+                11: ["0-month"],
                 12: ["2-month"],
             },
         },
@@ -188,14 +184,12 @@ SETTINGS = {
 
 
 CURRENT_DATE = date.today()
-CURRENT_Month = date.today().month
-
-# CURRENT_DATE=date.today() - timedelta(1) # to use yesterday's date
-
+#CURRENT_DATE=date.today() - timedelta(31) # to use last month forecast
+CURRENT_Month = CURRENT_DATE.month
 ### define file path for ICPAC forecast
-now_month = date.today() + relativedelta(months=-1)
-one_months = date.today() + relativedelta(months=+0)
-three_months = date.today() + relativedelta(months=+2)
+now_month = CURRENT_DATE + relativedelta(months=-1)
+one_months = CURRENT_DATE + relativedelta(months=+0)
+three_months = CURRENT_DATE + relativedelta(months=+2)
 
 One_Month = one_months.strftime("%b")
 Three_Month = three_months.strftime("%b")
@@ -207,22 +201,11 @@ CURRENT_Year = date.today().year
 file_name = f"{One_Month}-{Three_Month}_{Now_Month}{CURRENT_Year}"
 year_month = f"{CURRENT_Year}{Now_Month_nummeric}"
 
-Icpac_Forecast_FtpPath = (
-    "SharedData/gcm/seasonal/"
-    + f"{year_month}/"
-    + f"PredictedProbabilityRain_{file_name}.nc"
-)
-Icpac_Forecast_FtpPath_Rain = (
-    "SharedData/gcm/seasonal/" + f"{year_month}/" + f"PredictedRain_{file_name}.nc"
-)
 
 
 ####################
 ## OTHER SETTINGS ##
 ####################
-TRIGGER_PROBABILITY = 40
-TRIGGER_PROBABILITY_RAIN = 40
-SPI_Threshold_Prob = "0.16354"
 
 TRIGGER_LEVELS = {"minimum": 0.6, "medium": 0.7, "maximum": 0.8}
 
@@ -246,8 +229,15 @@ Icpac_Forecast_FilePath_Rain = (
 )
 
 
-fname = "Apr-Jun_Feb2022"
-YearMon = "202202"
+Icpac_Forecast_FtpPath = (
+    "SharedData/gcm/seasonal/"
+    + f"{year_month}/"
+    + f"PredictedProbabilityRain_{file_name}.nc"
+)
+
+Icpac_Forecast_FtpPath_Rain = (
+    "SharedData/gcm/seasonal/" + f"{year_month}/" + f"PredictedRain_{file_name}.nc"
+)
 
 #########################
 ## INPUT DATA SETTINGS ##
